1d6e637784439521f2a66858b36eb1007b7ffcae
Daniel Perez <tuvistavie@gmail.com>
2017-05-14 23:03:16 +0900

    Small fix

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,9 @@ BLOG_DIR=${BLOG_DIR:-/tmp/blog}
 BLOG_REPO=${BLOG_REPO:-"git@github.com:tuvistavie/blog.git"}
 
 NODE_ENV=production gulp build
-git clone $BLOG_REPO $BLOG_DIR
+if [ ! -d "$BLOG_DIR" ]; then
+  git clone $BLOG_REPO $BLOG_DIR
+fi
 rm -rf $BLOG_DIR/slides/$SLIDES_NAME
 cp -r build $BLOG_DIR/slides/$SLIDES_NAME
 
